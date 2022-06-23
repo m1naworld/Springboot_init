232c18cc0f099e95f42ef214085dac3b36bfe43e
mina <m1nna@naver.com>
2022-06-23 23:27:41 +0900

    MariaDB와 MyBatis 연동

---
+++ src/main/java/com/example/project/mapper/UserProfileMapper.java
@@ -0,0 +1,12 @@
+package com.example.project.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import com.example.project.model.UserProfile;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public class UserProfileMapper {
+
+}
